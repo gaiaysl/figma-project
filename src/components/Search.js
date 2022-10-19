dba750b0268f50abf09510123ad84df62968a505
Gaye Y <96841321+gaiaysl@users.noreply.github.com>
2022-10-19 22:40:23 +0300

    fixed

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,21 +3,22 @@ import { Input } from "@material-tailwind/react";
 export default function Search() {
     return (
       <div className="bg-green-400">
-        <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+        <div className="flex flex-row justify-center gap-1.5 py-12 px-4 sm:px-6 lg:flex lg:items-center  lg:py-16 lg:px-8">
           
-            <span className="block">Let’s go. Get a link 
-to download the app.</span>
-
-<div className="w-72 flex  ">
-      <Input />
+            <span className="">Let’s go. Get a link to download the app.</span>
+<div className="w-72 ">
+      <Input className="h-12 w-12 "  />
+     
     </div>
-              <a
+    <a
                 href="#"
                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-black px-5 py-3 text-base font-medium text-white"
               >
                APPLY TO DRIVE
               </a>
+   
             </div>
+           
         </div>
       
     )
